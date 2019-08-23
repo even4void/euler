ebe6ddff286d8d1ca0e722cc88fc2cee25cc0ecb
chl <chl@aliquote.org>
2019-08-23 13:56:31 +0200

    More Haskell

---
+++ 002.hs
@@ -0,0 +1,12 @@
+#! /usr/bin/env stack
+-- stack --resolver lts-13.26 script
+
+main :: IO ()
+main = print r
+
+r :: Integer
+r = sum $ filter even $ takeWhile (< 4000000) fib
+
+-- https://is.gd/NS9tqM
+fib :: [Integer]
+fib = 1 : 2 : zipWith (+) fib (tail fib)
