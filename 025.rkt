5d60751e99aa33373f04f159ead3429af22bf3dc
chl <chl@aliquote.org>
2019-09-10 17:11:03 +0200

    Add problem statement (Fix #1)

--- 025.rkt
+++ 025.rkt
@@ -1,5 +1,11 @@
 #lang racket
 
+;; Problem 25
+;; The 12th term, F12, is the first term to contain three digits.
+;; What is the index of the first term in the Fibonacci sequence
+;; to contain 1000 digits?
+;; https://projecteuler.net/problem=25
+
 ;; NOTE Some nice Clojure code here: https://is.gd/PxMObM
 
 (define (length n)
