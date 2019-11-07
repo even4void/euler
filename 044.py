8ab34d1eddce012c01163a979c6b33d6fa936924
chl <chl@aliquote.org>
2019-11-07 17:08:38 +0100

    ↻ wip

---
+++ 044.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import math
+
+
+def is_pentagonal(n):
+    n = (math.sqrt(1 + 24*n) + 1) / 6
+    return n == int(n)
+
+
+r, i = 0, 1
+
+while True:
+    i += 1
+    n = i * (3*i - 1) // 2
+    j = i - 1
+    while j > 0:
+        print(j)
+        m = j * (3*j - 1) // 2
+        j -= 1
+        if is_pentagonal(n - m) and is_pentagonal(n + m):
+            r = n - m
+            break
+
+print(r)
