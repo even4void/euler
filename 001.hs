3c755c6e872ae2075da453cd6434f8784ebfd97b
chl <chl@aliquote.org>
2020-12-30 21:12:17 +0100

    Udate Stack LTS

--- 001.hs
+++ 001.hs
@@ -1,5 +1,5 @@
 #! /usr/bin/env stack 
--- stack --resolver lts-13.26 script
+-- stack --resolver lts-16.26 script
 
 main = print r
 r = sum [x | x <- [0..999], mod x 3 == 0 || mod x 5 == 0]
--- 002.hs
+++ 002.hs
@@ -1,5 +1,5 @@
 #! /usr/bin/env stack
--- stack --resolver lts-13.26 script
+-- stack --resolver lts-16.26 script
 
 main :: IO ()
 main = print r
